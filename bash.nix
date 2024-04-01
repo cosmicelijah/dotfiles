19467d6ca925f007b26b67fe7a8a181316155807
cosmicelijah <cosmicelijah@gmail.com>
2024-03-31 21:41:12 -0400

    Nixify bashrc a little bit better

--- bash.nix
+++ bash.nix
@@ -8,12 +8,12 @@
 
   	historyControl = ["ignoredups"];
 
-  	initExtra = [
-  	  "PS1='\[\e[1;33m\]{\[\e[1;31m\]\u\[\e[1;32m\]@\[\e[1;34m\]\h\[\e[1;33m\]}-\[\e[1;33m\][\[\e[1;35m\]\w\[\e[1;33m\]]\[\e[1;37m\]\$\[\e[0;37m\] '"
-      "PS2='\[\e[1;35m\]>\[\e[0;37m\] '"
-	  "cat ~/.bash_header | cowsay | lolcat"
-	  "shopt -s autocd"
-  	];
+  	initExtra = ''
+  	  PS1='\[\e[1;33m\]{\[\e[1;31m\]\u\[\e[1;32m\]@\[\e[1;34m\]\h\[\e[1;33m\]}-\[\e[1;33m\][\[\e[1;35m\]\w\[\e[1;33m\]]\[\e[1;37m\]\$\[\e[0;37m\] '
+      PS2='\[\e[1;35m\]>\[\e[0;37m\] '
+	  cat ~/.bash_header | cowsay | lolcat
+	  shopt -s autocd
+  	'';
 
   	profileExtra = [
   	  ". ~/.bash_aliases"
